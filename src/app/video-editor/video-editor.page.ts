f0af937a039d1618c511df367a6a7ae7357d5be0
HanweiCarlao <carlos4349@gmail.com>
2025-05-05 19:56:48 +0800

    export button updated

--- src/app/video-editor/video-editor.page.ts
+++ src/app/video-editor/video-editor.page.ts
@@ -20,9 +20,7 @@ export class VideoEditorPage implements OnInit, AfterViewInit {
   @ViewChild('videoPlayer', { static: true })
   videoPlayer!: ElementRef<HTMLVideoElement>;
 
-  // ← Updated to use your video file
   videoSrc = 'assets/videos/annie_wave_to_earth.mp4';
-
   isPlaying = false;
   duration = 0;
   currentTime = 0;
@@ -35,23 +33,13 @@ export class VideoEditorPage implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     const vid = this.videoPlayer.nativeElement;
-
-    vid.onloadedmetadata = () => {
-      this.duration = vid.duration;
-    };
-
-    vid.ontimeupdate = () => {
-      this.currentTime = vid.currentTime;
-    };
+    vid.onloadedmetadata = () => (this.duration = vid.duration);
+    vid.ontimeupdate = () => (this.currentTime = vid.currentTime);
   }
 
   togglePlay() {
     const vid = this.videoPlayer.nativeElement;
-    if (this.isPlaying) {
-      vid.pause();
-    } else {
-      vid.play();
-    }
+    this.isPlaying ? vid.pause() : vid.play();
     this.isPlaying = !this.isPlaying;
   }
 
@@ -70,10 +58,7 @@ export class VideoEditorPage implements OnInit, AfterViewInit {
   }
 
   exportVideo() {
-    alert(
-      `Exporting clip from ${this.startTime.toFixed(1)}s to ${this.endTime.toFixed(
-        1
-      )}s…`
-    );
+    // Navigate to your Upload Info page
+    this.navCtrl.navigateForward('/upload-info');
   }
 }
