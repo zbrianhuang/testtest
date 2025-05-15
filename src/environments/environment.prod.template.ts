ab1ebc8ed2f83b268a0b73d699e6cd7cba868b62
zbrianhuang <67428806+zbrianhuang@users.noreply.github.com>
2025-05-15 21:18:31 +0800

    oops

--- src/environments/environment.prod.template.ts
+++ src/environments/environment.prod.template.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  production: true,
+  production: false,
   aws: {
-    accessKeyId: 'YOUR_AWS_ACCESS_KEY_ID',
-    secretAccessKey: 'YOUR_AWS_SECRET_ACCESS_KEY',
-    region: 'YOUR_AWS_REGION',
-    bucketName: 'YOUR_AWS_BUCKET_NAME'
+    accessKeyId: '',
+    secretAccessKey: '',
+    region: '',
+    bucketName: ''
   }
-}; 
+};
